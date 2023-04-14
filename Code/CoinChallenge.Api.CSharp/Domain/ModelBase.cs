fe304a33d601c8c96c1d83be7d31f251b24a0b16
Stephen R. Peterson <srpeterson@outlook.com>
2023-04-14 16:45:43 -0700

    Removed projects that will upgrade

--- Code/CoinChallenge.Api.CSharp/Domain/ModelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoinChallenge.Api.CSharp.Domain
-{
-    public abstract class ModelBase
-    {
-        public OperationResult OperationResult { get; internal set; }
-
-        public string Message { get; internal set; } = string.Empty;
-    }
-}
