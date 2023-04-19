949f790fdaa462ba9991f3abcde190240a37f963
Stephen Peterson <speterson@evestment.com>
2023-04-18 17:59:59 -0700

    WIP

--- Projects/FSharp.Examples/Scripts/FsToolKit.fsx
+++ Projects/FSharp.Examples/Scripts/FsToolKit.fsx
@@ -1,6 +1,25 @@
+#r "nuget: FSToolkit.ErrorHandling" 
+
+open FsToolkit.ErrorHandling
 open System
 
-module Validation = 
+[<AutoOpen>]
+module DomainTypes = 
+
+    type BenchmarkType = 
+        | AbsoluteReturnBenchmark
+        | AssetWeightedBenchmark
+        | CompositeReturnBenchmark
+        | CompositeWeightedBenchmark
+        | PullForwardBenchmark
+        | StaticWeightedBenchmark
+
+    type ValidBenchmarkName =  ValidBenchmarkName of string
+    type ValidBenchmarkTypeId = ValidBenchmarkTypeId of byte
+    type LagDay = { AsDateOnly: DateOnly;  DaysSinceEpoch: int } 
+    type ValidLagDay = ValidLagDay of LagDay
+
+module BenchmarkValidation = 
 
     let validate validator failure input =
         if validator input then Ok input else Error failure
@@ -8,10 +27,8 @@ module Validation =
     let validateOption validator failure (input: 'a option) =
         if validator input then Ok (input |> Option.get) else Error failure
 
-type ValidBenchmarkName =  private ValidBenchmarkName of string
-
 module BenchmarkName =
-    open Validation
+    open BenchmarkValidation
 
     [<Literal>]
     let private MaxNameLength = 255
@@ -24,30 +41,17 @@ module BenchmarkName =
         let predicate (s: string) = s.Length < MaxNameLength
         name |> validate predicate $"Name exceeds max character length of '{MaxNameLength}'"
 
-    let create (name: string option) = 
-        let isValidName = 
-            Ok name
-            |> Result.bind nameIsNone
-            |> Result.bind nameIsTooLong
-        match isValidName with
-        | Ok s -> Ok (ValidBenchmarkName s)
-        | Error err -> Error err
+    let create (name: string option) =
+         result {
+            let! _ = name |> nameIsNone
+            let! _ = name |> Option.traverseResult  nameIsTooLong
+            return  ValidBenchmarkName (name |> Option.get)
+        }
 
     let value (ValidBenchmarkName s) = s
 
-type ValidBenchmarkTypeId = private ValidBenchmarkTypeId of byte
-
 module BenchmarkTypeId =
-    open Validation
-
-    // this goes in domain
-    type BenchmarkType = 
-        | AbsoluteReturnBenchmark
-        | AssetWeightedBenchmark
-        | CompositeReturnBenchmark
-        | CompositeWeightedBenchmark
-        | PullForwardBenchmark
-        | StaticWeightedBenchmark
+    open BenchmarkValidation
 
     let getBenchmarkTypeId benchamarkType =
         match benchamarkType with
@@ -61,95 +65,48 @@ module BenchmarkTypeId =
     let create (createBenchmarkDtoType: BenchmarkType) (typId: byte) = 
         let benchmarkTypeId = createBenchmarkDtoType |> getBenchmarkTypeId
         let predicate (id: ValidBenchmarkTypeId) = id = ValidBenchmarkTypeId benchmarkTypeId
-        (ValidBenchmarkTypeId typId) |> validate predicate $"Invalid TypeId"
+        (ValidBenchmarkTypeId typId) |> validate predicate $"Invalid Benchmark TypeId"
 
     let value (ValidBenchmarkTypeId s) = s
 
-type LagDay = { AsDateOnly: DateOnly;  DaysSinceEpoch: int } 
-type ValidLagDay = private ValidLagDay of LagDay
-
 module LagDay =
-    open Validation
+    open BenchmarkValidation
 
     let private ``date is before january 1 1900`` (lagDate: DateOnly) = 
         let predicate (value: DateOnly) = value >= new DateOnly (1900, 1, 1)
         lagDate |> validate predicate "Date can not be before Jaunary 1, 1900"
 
-    let private iEndOfMonth (lagDate: DateOnly) = 
+    let private isEndOfMonth (lagDate: DateOnly) = 
         let predicate (value: DateOnly)  =  DateTime.DaysInMonth(value.Year, value.Month) = value.Day;
         lagDate |> validate predicate "Lag Date must be end of month"
-
-    let create (lagDate: DateOnly) =
-        let isValidLagDate = 
-            Ok lagDate
-            |> Result.bind ``date is before january 1 1900``
-            |> Result.bind iEndOfMonth
-        match isValidLagDate with
-        | Ok s -> Ok (ValidLagDay {AsDateOnly = s; DaysSinceEpoch = 5})
-        | Error err -> Error err
+    
+    let create (lagDate: DateOnly option) =
+        result {
+            let! _ = lagDate |> Option.traverseResult ``date is before january 1 1900``
+            let! _ = lagDate |> Option.traverseResult isEndOfMonth
+            return  Some (ValidLagDay { AsDateOnly = (lagDate |> Option.get); DaysSinceEpoch = 5})
+        }
 
     let value (ValidLagDay s) = s
 
-module Example =
-    open BenchmarkName
-    open BenchmarkTypeId
-    open LagDay
+module CreateValidatedABsoluteReturnDto =
 
-    type UnValidatedDto = { Name: string option; TypeId: byte; LagDate: DateOnly option }
-    type ValidatedDto = { Name: ValidBenchmarkName; TypeId: ValidBenchmarkTypeId; LagDate: ValidLagDay option  }
+    // Pretend that this is the Dto passed to us from enpoint
+    type UnValidatedDto = { Name: string option; TypeId: byte; (* A bunch of other properties ..*) LagDate: DateOnly option }
 
-    let unValidatedDto: UnValidatedDto = { Name = (Some "Steve"); TypeId = 3uy; LagDate = Some (new DateOnly(2022, 12,31)) }
+    // This is the Dto that has all the properties validated and will use for rest of code
+    type ValidatedDto = { Name: ValidBenchmarkName; TypeId: ValidBenchmarkTypeId; LagDate: ValidLagDay option }
 
-    //let fred = BenchmarkName.create unValidatedDto.Name
+    // This validates the properties and returns a a ValidatedDto or a list of errors 
+    let toValidatedDto (unValidatedDto: UnValidatedDto) = 
 
-    //let ggg = 
-    //    let foo = 
-    //        match fred with
-    //        | Ok s ->  BenchmarkName.value s
-    //        | Error err -> err
-    //    5
+        validation {
+            let! validName = BenchmarkName.create unValidatedDto.Name
+            and! validHeight = BenchmarkTypeId.create BenchmarkType.AbsoluteReturnBenchmark unValidatedDto.TypeId
+            and! validDob = LagDay.create unValidatedDto.LagDate
 
-    //let fred2 = BenchmarkTypeId.create BenchmarkType.AbsoluteReturnBenchmark unValidatedDto.TypeId
+            return { Name = validName; TypeId = validHeight; LagDate = validDob }
+        }
 
-    //let ggg3 = 
-    //    let foo = 
-    //        match fred2 with
-    //        | Ok s ->  BenchmarkTypeId.value s
-    //        | Error err -> err
-    //    5
-
-    //let fred3 = 
-    //    match unValidatedDto.LagDate with 
-    //    | Some s -> Some (LagDay.create s)
-    //    | None -> None
-
-    //let foo = fred |> Result.bind fred2
-
-    //3
-
-    //here I want to go from UnvalidatedDto to ValidatedDto
     
-    //type transform = UnValidatedDto -> ValidatedDto
-
-    //type UnValidatedName = UnValidatedName of string
-    //type ValidatedName = ValidatedName of string
-
-    //type UnValidatedDto = {Name: UnValidatedName; TypeId: byte }
-    //type ValidatedDto = {Name: ValidatedName; TypeId: byte}
-
-    //type Benchmark = UnValidatedDto of UnValidatedDto | ValidatedDto of ValidatedDto
-
-    //let unValidatedDto: UnValidatedDto = {Name = UnValidatedName "Steve"; TypeId = 3uy}
-    //let validatedDto: ValidatedDto = {Name = ValidatedName "Steve"; TypeId = 3uy }
-
-    //type transformName = UnValidatedName -> Result<ValidatedName, string>
-
-    //let validateName (unValidatedName: UnValidatedName) =
-    //    let (UnValidatedName name) = unValidatedName
-    //    if name.Length < 255 then Ok (ValidatedName name) else Error "Invalid name"
-     
-    //let convert (f: transformName) (t: UnValidatedDto) =
-    //    let bob = f t.Name
-    //    match bob with
-    //    | Ok s ->  Some (ValidatedDto {Name = s; TypeId = 3uy })
-    //    | Error s ->  None
+    let testIt = toValidatedDto { Name = (Some "Steve"); TypeId = 1uy; LagDate = Some (new DateOnly(2022, 12, 31)) }
