8ca26d481f53737510409fe538ce12096809279c
Stephen Peterson <speterson@evestment.com>
2023-04-19 14:11:47 -0700

    WIP

--- Projects/CSharp.Examples/Composition.cs
+++ Projects/CSharp.Examples/Composition.cs
@@ -2,7 +2,25 @@
 
 public class Composition
 {
-    public int SubtractOneSquareAddTen(int input)
+    // 'Normal' imperative way
+    public static int SubtractOneSquareAddTen1(int value)
+    {
+        int valueMinusOne = value - 1; 
+        int valueSquared = valueMinusOne * valueMinusOne; 
+        int result = valueSquared + 10; 
+        return result;
+    }
+
+    // Imperative way but all squished together..
+    // Is it readable for the next guy or does he have to go dust off
+    // his 8th grade algebra book?
+    public static int SubtractOneSquareAddTen2(int value)
+    {
+        return ((value - 1) * (value - 1)) + 10;
+    }
+
+    // Better - a C# way of doing F# piping..
+    public static int SubtractOneSquareAddTen3(int input)
     {
         return input
             .SubtractOne()
