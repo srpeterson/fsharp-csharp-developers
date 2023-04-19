6c18a1c071a8faae011ad95bf88bfa8a0e75e947
Stephen Peterson <speterson@evestment.com>
2023-04-19 10:32:09 -0700

    WIP

--- Projects/FSharp.Examples/Composition.fs
+++ Projects/FSharp.Examples/Composition.fs
@@ -6,24 +6,22 @@ module Composition =
     let square input = input * input
     let addTen input = input + 10
        
-    //the '|>' operator allows us to supply a value to the
-    //last parameter of another function. This allows us to chain
-    //functions together just as long as the output of the "giver"
-    //function matches the LAST argument of the "reciever' function
-    let subtractOneSquareAddTenPiped input =  // signature: int -> string
+    // the '|>' operator allows us to supply a value to the
+    // last parameter of another function. This allows us to chain
+    // functions together just as long as the output of the "giver"
+    // function matches the LAST argument of the "reciever' function
+    let subtractOneSquareAddTenPiped input =  // signature: int -> int
         input 
         |> subtractOne 
         |> square 
         |> addTen
     
-    //the '>>' operator allows us to combine one or more functions
-    //together into one function, just as long as the output of the "giver" function
-    //is the same as the input of the "reciever" function
-    let subtractOneSquareAddTenComposed = subtractOne >> square >> addTen // signature: int -> string
+    // the '>>' operator allows us to combine one or more functions to a single function
+    // just as long as the output of the "giver" function
+    // is the same as the input of the "reciever" function
+    let subtractOneSquareAddTenComposed = subtractOne >> square >> addTen // signature: (int -> int)
 
     let gimmeOddNumbers integers = 
         integers 
         |> List.map (subtractOneSquareAddTenComposed) // can't do this with piped
         |> List.filter(fun i -> i % 2 <> 0)
-
-
--- Projects/FSharp.Examples/Scripts/Composition.fsx
+++ Projects/FSharp.Examples/Scripts/Composition.fsx
@@ -4,11 +4,11 @@ let square input = input * input
 let addTen input = input + 10
 let theAnswer prefix input = sprintf "%s %d" prefix input
    
-//the '|>' operator allows us to supply a value to the
-//last parameter of another function. This allows us to chain
-//functions together just as long as the output of the "giver"
-//function matches the LAST argument of the "reciever' function
-let whatIsTheAnswerPiped input =  // signature: int -> string
+// the '|>' operator allows us to supply a value to the
+// last parameter of another function. This allows us to chain
+// functions together just as long as the output of the "giver"
+// function matches the LAST argument of the "reciever' function
+let whatIsTheAnswerPiped input =  // signature: int -> int
     input 
     |> subtractOne 
     |> square 
@@ -17,10 +17,10 @@ let whatIsTheAnswerPiped input =  // signature: int -> string
 
 let answerToTenPiped = whatIsTheAnswerPiped 10
 
-//the '>>' operator allows us to combine one or more functions
-//together into one function, just as long as the output of the "giver" function
-//is the same as the input of the "reciever" function
-let whatIsTheAnswerComposed = //signature: (int -> string)
+// the '>>' operator allows us to combine one or more functions to a single function
+    // just as long as the output of the "giver" function
+    // is the same as the input of the "reciever" function
+let whatIsTheAnswerComposed = //signature: (int -> iint)
     subtractOne >> square >> addTen >> theAnswer "The composed answer is:"
 
 let answerToTenComposed = whatIsTheAnswerComposed 10
